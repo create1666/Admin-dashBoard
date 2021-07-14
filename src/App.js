38490d48b721c99b9eafafca8518941261eadbb4
create1666 <create1666@gmail.com>
2021-07-14 15:17:15 +0100

    User page URL routing , with material Ui data-grid layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-
-import './App.css'
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar';
-import Home from './Pages/Home';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import UserList from "./Pages/Userpage/UserList";
 function App() {
   return (
     <div className="app">
-       <NavBar/>
-       <div className="app__container">
-       <SideBar/>
-        <Home/>
-       </div>
-      
+      <Router>
+        <NavBar />
+        <div className="app__container">
+          <SideBar />
+          <Switch>
+            <Route path="/Users">
+              <UserList />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/WIdgets/WidgetLg/WidgetLg.jsx
+++ src/components/WIdgets/WidgetLg/WidgetLg.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import './WidgetLg.css'
+import React from "react";
+import "./WidgetLg.css";
 
 export default function WidgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgStatus " + type}>{type}</button>;
+  };
   return (
-    <div className='widgetLg'>
-      large
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTrow">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+
+        <tr className="widgetLgTrow">
+          <td className="widgetLgTdUser">
+            <img src="https://images.pexels.com/photos/2876486/pexels-photo-2876486.png?cs=srgb&dl=pexels-shan-patel-2876486.jpg&fm=jpg" alt="" className="widgetLgImg" />
+            <span className="widgetLgUsername"> John</span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgDate">12 Jul 2021</span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgAmount">$500.00</span>
+          </td>
+          <td className="widgetLgTdBtn">
+           
+            <Button type={"Approved"} />
+          </td>
+        </tr>
+
+        <tr className="widgetLgTrow">
+          <td className="widgetLgTdUser">
+            <img  src="https://images.pexels.com/photos/2876486/pexels-photo-2876486.png?cs=srgb&dl=pexels-shan-patel-2876486.jpg&fm=jpg" alt="" className="widgetLgImg" />
+            <span className="widgetLgUsername">Boluwaji </span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgDate">12 Jul 2021</span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgAmount">$500.00</span>
+          </td>
+          <td className="widgetLgTdBtn">
+            <Button type={"Pending"} />
+          </td>
+        </tr>
+
+        <tr className="widgetLgTrow">
+          <td className="widgetLgTdUser">
+            <img  src="https://images.pexels.com/photos/2876486/pexels-photo-2876486.png?cs=srgb&dl=pexels-shan-patel-2876486.jpg&fm=jpg" alt="" className="widgetLgImg" />
+            <span className="widgetLgUsername">Boluwaji </span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgDate">12 Jul 2021</span>
+          </td>
+          <td className="widgetLgTd">
+            <span className="widgetLgAmount">$500.00</span>
+          </td>
+          <td className="widgetLgTdBtn">
+          <Button type={"Declined"} />
+
+          </td>
+        </tr>
+      </table>
     </div>
-  )
+  );
 }
